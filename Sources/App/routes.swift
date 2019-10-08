3d5275fd9d681ca98ea792f096e5578f96908d74
artIU7 <stratienko.artem@gmail.com>
2019-10-09 00:16:56 +0300

    change path route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,16 +2,11 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
+   
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("api") { req in
         return "Hello, world!"
     }
-
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
