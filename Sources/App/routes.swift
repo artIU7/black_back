b5d65f68042c7a0339cf98b340b299e2edec21e5
artIU7 <stratienko.artem@gmail.com>
2019-10-14 11:44:19 +0300

    add new dependencies

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,6 +8,11 @@ struct oldArea : Content {
     var lon : Double
     var lan : Double
     var range : Int
+   /* init structure oldArea : Content
+     * header struct oldArea for json->route
+     * with Future <oldArea> /*Future*/
+     * where Future :: oldArea
+    */
     init(_ radio : String ,_ lon : Double,_ lan : Double,_ range : Int) {
         self.radio = radio
         self.lon = lon
@@ -16,14 +21,13 @@ struct oldArea : Content {
     }
 }
 var objectArea = oldArea("LTE", 43.5, 39.6, 1500)
-
+/*comment structure*/
+    //# oldarea(struct(area: Int))
 
 public func routes(_ router: Router) throws {
-   
     // Basic "Hello, world!" example
     router.get("api","v3","oldarea"/*, Request*/) { req in
-        //
-        return "ggg"
+    return "ggg"
     }
     //
   /*  router.get("api","v3","oldarea","json") { req -> Future<String> in
