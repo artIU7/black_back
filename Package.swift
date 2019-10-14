4979a393ce5f19a457a2c907b610c14cd92cf4e1
artIU7 <stratienko.artem@gmail.com>
2019-10-14 11:10:57 +0300

    change error

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url:"https://github.com/vapor/leaf.git",.upToNextMinor(from: "3.0.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor","Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
